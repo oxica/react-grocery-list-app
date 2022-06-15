1153b1dcde52605883707eef0cae94ad187dc464
Oksana <kskaplun@gmail.com>
2022-06-15 15:40:30 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,32 @@
 import { useState } from 'react';
-import { useGetGoodsQuery } from '../redux';
+import { useAddProductMutation, useGetGoodsQuery } from '../redux';
 
 export const App = () => {
   const [count, setCount] = useState();
+  const [newProduct, setNewProduct] = useState();
   const { data = [], isLoading } = useGetGoodsQuery(count);
+  const [addProduct, { isError }] = useAddProductMutation();
+
+  const handleAddProduct = async () => {
+    if (newProduct) {
+      await addProduct({ name: newProduct }).unwrap();
+      setNewProduct('');
+    }
+  };
 
   if (isLoading) return <p>Loading...</p>;
 
   return (
     <center>
       <h1>My grocery list</h1>
+      <div>
+        <input
+          type="text"
+          value={newProduct}
+          onChange={e => setNewProduct(e.target.value)}
+        />
+        <button onClick={handleAddProduct}>Add</button>
+      </div>
       <div>
         <select value={count} onChange={e => setCount(e.target.value)}>
           <option value="''">all</option>
