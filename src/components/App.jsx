532ba2548782b40d082db5ae01ab3268ccfb8d81
Oksana <kskaplun@gmail.com>
2022-06-15 15:26:00 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import { useGetGoodsQuery } from '../redux';
 
 export const App = () => {
-  const { data = [], isLoading } = useGetGoodsQuery();
+  const [count, setCount] = useState();
+  const { data = [], isLoading } = useGetGoodsQuery(count);
 
   if (isLoading) return <p>Loading...</p>;
 
   return (
     <center>
       <h1>My grocery list</h1>
+      <div>
+        <select value={count} onChange={e => setCount(e.target.value)}>
+          <option value="''">all</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+      </div>
       <ul>
         {data.map(item => (
           <li key={item.id}>{item.name}</li>
